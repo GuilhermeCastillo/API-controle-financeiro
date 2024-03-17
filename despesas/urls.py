ad5ddba436f73f65006d48ce686e733f30e25232
castilho <guilherme.castilhot2c18@gmail.com>
2024-03-17 11:38:12 -0300

    listagem por mes despesas

--- despesas/urls.py
+++ despesas/urls.py
@@ -3,5 +3,14 @@ from . import views
 
 urlpatterns = [
     path("despesas/", views.DespesaCreateListView.as_view(), name="receita"),
-    path("despesas/<int:pk>/", views.DespesaRetrieveUpdateDestroyView.as_view(), name="receita-detail-view")
+    path(
+        "despesas/<int:pk>/",
+        views.DespesaRetrieveUpdateDestroyView.as_view(),
+        name="receita-detail-view",
+    ),
+    path(
+        "despesas/<int:year>/<int:month>/",
+        views.DespesaCreateListView.as_view(),
+        name="despesa-detail-date",
+    ),
 ]
--- despesas/views.py
+++ despesas/views.py
@@ -9,9 +9,14 @@ class DespesaCreateListView(generics.ListCreateAPIView):
     def get_queryset(self):
         queryset = Despesa.objects.all()
         descricao = self.request.query_params.get("descricao")
+        year = self.kwargs.get("year")
+        month = self.kwargs.get("month")
 
         if descricao is not None:
             queryset = queryset.filter(descricao=descricao)
+
+        if (year is not None) and (month is not None):
+            queryset = Despesa.objects.filter(data__year=year, data__month=month)
 
         return queryset
 
