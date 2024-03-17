bad2a48884ceff38ae4811b9f81743159e8fee2b
castilho <guilherme.castilhot2c18@gmail.com>
2024-03-17 11:34:04 -0300

    listagem por mes

--- receitas/urls.py
+++ receitas/urls.py
@@ -3,5 +3,14 @@ from . import views
 
 urlpatterns = [
     path("receitas/", views.ReceitaCreateListView.as_view(), name="receita"),
-    path("receitas/<int:pk>/", views.ReceitaRetrieveUpdateDestroyView.as_view(), name="receita-detail-view")
+    path(
+        "receitas/<int:pk>/",
+        views.ReceitaRetrieveUpdateDestroyView.as_view(),
+        name="receita-detail-view",
+    ),
+    path(
+        "receitas/<int:year>/<int:month>/",
+        views.ReceitaCreateListView.as_view(),
+        name="receita-detail-date",
+    ),
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -9,9 +9,14 @@ class ReceitaCreateListView(generics.ListCreateAPIView):
     def get_queryset(self):
         queryset = Receita.objects.all()
         descricao = self.request.query_params.get("descricao")
+        year = self.kwargs.get("year")
+        month = self.kwargs.get("month")
 
         if descricao is not None:
             queryset = queryset.filter(descricao=descricao)
+
+        if (year is not None) and (month is not None):
+            queryset = Receita.objects.filter(data__year=year, data__month=month)
 
         return queryset
 
