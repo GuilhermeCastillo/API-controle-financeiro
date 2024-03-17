ef148c3dc5c89ad1940d132ea40c6d82fdbd8cf9
castilho <guilherme.castilhot2c18@gmail.com>
2024-03-17 10:34:43 -0300

    query param aplicado no app de receitas

--- receitas/views.py
+++ receitas/views.py
@@ -2,10 +2,22 @@ from receitas.models import Receita
 from rest_framework import generics
 from receitas.serializers import ReceitaModelSerializer
 
+
 class ReceitaCreateListView(generics.ListCreateAPIView):
-    queryset = Receita.objects.all()
     serializer_class = ReceitaModelSerializer
-    
+
+    def get_queryset(self):
+        queryset = Receita.objects.all()
+        descricao = self.request.query_params.get("descricao")
+
+        print(descricao)
+
+        if descricao is not None:
+            queryset = queryset.filter(descricao=descricao)
+
+        return queryset
+
+
 class ReceitaRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Receita.objects.all()
     serializer_class = ReceitaModelSerializer
